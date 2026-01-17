526549f0b97a86f2e8fc1daab7acb37cca53547f
Amayuru1999 <amarasingheau@gmail.com>
2026-01-17 15:51:11 +0530

    chore: Database migrations for newly added data

--- WebApplication1/Data/Context.cs
+++ WebApplication1/Data/Context.cs
@@ -1,8 +1,56 @@
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Data;
 
 public class Context : DbContext
 {
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Book>().HasData(
+            new Book
+        {
+            Id = 1,
+            Title = "Book 1",
+            Author = "John Doe",
+            YearPublished = 2000
+        },
+        new Book
+        {
+            Id = 2,
+            Title = "Book 2",
+            Author = "John Doe",
+            YearPublished = 2001
+        },
+        new Book
+        {
+            Id = 3,
+            Title = "Book 3",
+            Author = "John Doe",
+            YearPublished = 2002
+        },
+        new Book
+        {
+            Id = 4,
+            Title = "Book 4",
+            Author = "John Doe",
+            YearPublished = 2003
+        },
+        new Book
+            {
+                Id = 5,
+                Title = "Book 5",
+                Author = "John Doe",
+                YearPublished = 2004
+            }
+            );
+    }
     
+    public DbSet<Book> Books { get; set; }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
